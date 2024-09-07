9475138b4446c38837b3d6010f1f5ef49eab34ec
Lovro Terseglav <lovroterseglav@protonmail.com>
2024-09-07 11:07:24 +0200

    Add instructions to enable hook

--- post-commit-hook.sh
+++ post-commit-hook.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# To enable it run, to disable delete the symlink
+# $ ln -s post-commit-hook.sh .git/hooks/post-commit
+
 while IFS= read -r -d $'\0' ARG; do
     if test "$ARG" == '--no-verify'; then
         exit 0
