a1d8eb1af4f7b2ad895c0f5b55ac4d60d5d5f301
Lovro Terseglav <lovroterseglav@protonmail.com>
2024-09-07 11:03:38 +0200

    update commit time script

--- commit-time.sh
+++ commit-time.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 date > commit-time.txt
 git add commit-time.txt
-git commit --amend --no-edit --no-verify commit-time.txt
+git commit --amend --no-edit -C HEAD --no-verify commit-time.txt
